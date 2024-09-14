222cf3cf304340cc4a1e20fd3cb6508e9aea42b3
Thamira2001 <thamira705@gmail.com>
2024-09-14 15:01:56 -0500

    added new script that loads accss token for HF and uses inference api to send request toi model gpt2 and gets response back to test if api and Hf works and it does

---
+++ init_testing/scripts/hf-api-test.py
@@ -0,0 +1,30 @@
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+import requests
+# from langchain import HuggingFaceHub
+
+
+# Load environment variables from the .env file
+dotenv_path = Path('../../.env')
+load_dotenv(dotenv_path=dotenv_path)
+
+# Access the API token from the environment variables
+api_token = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+
+print(api_token) #check if .env file is importing the access token for huging face hub properly 
+
+headers = {"Authorization": f"Bearer {api_token}"}
+API_URL = "https://api-inference.huggingface.co/models/gpt2"
+
+def query_huggingface_api(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+
+input_text = "can you tell who lebron james is ?"
+response = query_huggingface_api({"inputs": input_text})
+
+print(response)
+
+
+
